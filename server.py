bde88093ceb6583be764bd776eaeadd37aa1d13c
Mayre <Mayrenapine30@outlook.es>
2024-09-04 08:34:10 -0600

    Aplicacion academica

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 from urllib import parse
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
-class ServidorBasico(SimpleHTTPRequestHandler):
+class servidorBasico(SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
         self.wfile.write("Hola Mundo".encode())
 
-server = HTTPServer(('Localhost',3000), ServidorBasico)
+server = HTTPServer(('localhost', 3000), servidorBasico)
 server.serve_forever()
-print("servidor ejecutado en el puerto 3000")
+print("Servidor ejecutado en el puerto 3000")
